67b00abf6c432490dc9d756e43187d140826d3e9
Mark Colby <macolby14@gmail.com>
2021-05-25 16:05:23 -1000

    add flags and pricing

--- src/components/ShoeCard/ShoeCard.js
+++ src/components/ShoeCard/ShoeCard.js
@@ -36,14 +36,17 @@ const ShoeCard = ({
       <Wrapper>
         <ImageWrapper>
           <Image alt="" src={imageSrc} />
+          {variant==='on-sale' && <SaleFlag>On sale</SaleFlag>}
+          {variant==='new-release' && <NewFlag>New</NewFlag>} 
         </ImageWrapper>
         <Spacer size={12} />
         <Row>
           <Name>{name}</Name>
-          <Price>{formatPrice(price)}</Price>
+          <Price style ={{'--color': variant === 'on-sale' ? COLORS.gray[700] : undefined, '--text-decoration': variant === 'on-sale' ? 'line-through' : undefined}}>{formatPrice(price)}</Price>
         </Row>
         <Row>
           <ColorInfo>{pluralize('Color', numOfColors)}</ColorInfo>
+          {variant === 'on-sale' ? <SalePrice>{formatPrice(salePrice)}</SalePrice> : undefined }
         </Row>
       </Wrapper>
     </Link>
@@ -79,7 +82,10 @@ const Name = styled.h3`
   color: ${COLORS.gray[900]};
 `;
 
-const Price = styled.span``;
+const Price = styled.span`
+  color: var(--color);
+  text-decoration: var(--text-decoration);
+`;
 
 const ColorInfo = styled.p`
   color: ${COLORS.gray[700]};
@@ -88,6 +94,28 @@ const ColorInfo = styled.p`
 const SalePrice = styled.span`
   font-weight: ${WEIGHTS.medium};
   color: ${COLORS.primary};
+`;
+
+const Flag = styled.div`
+  position: absolute;
+  top: 12px;
+  right: -4px;
+  height: 32px;
+  border-radius: 2px;
+  padding: 0 10px;
+  font-size: ${14/16}rem;
+  font-weight: ${WEIGHTS.bold};
+  color: ${COLORS.white};
+  display: flex;
+  align-items: center;
+`;
+
+const SaleFlag = styled(Flag)`
+    background-color: ${COLORS.primary};
+`;
+
+const NewFlag = styled(Flag)`
+  background-color: ${COLORS.secondary};
 `;
 
 export default ShoeCard;
